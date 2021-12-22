9783c37eb88672721fe1a33004386e4c1fc33301
chunyoung0518@gmail.com <chunyoung0518@gmail.com>
2021-12-22 14:06:42 +1100

    use loggerService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {Controller, Get, Inject, LoggerService} from '@nestjs/common';
 import { AppService } from './app.service';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import {WINSTON_MODULE_NEST_PROVIDER, WINSTON_MODULE_PROVIDER} from 'nest-winston';
 import { Logger } from 'winston';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    @Inject(WINSTON_MODULE_PROVIDER) private readonly winstonLogger: Logger,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly winstonLogger: LoggerService,
   ) {}
 
   @Get()
   getHello(): string {
-    this.winstonLogger.info('This is just an info');
+    this.winstonLogger.log('This is just an info');
     this.winstonLogger.warn('This is just a warning');
     this.winstonLogger.error('This is an error');
     this.winstonLogger.debug('This is a debug message');
